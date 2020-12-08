9025563d971003aa29c1e796c618a69669bdc3ab
Filip Lindahl <filip.g.lindahl@gmail.com>
2020-12-08 23:02:06 +0100

    cleaned up day 8 and timed it

--- day8/day8.go
+++ day8/day8.go
@@ -7,7 +7,15 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
+
+func elapsed(what string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n\n", what, time.Since(start))
+	}
+}
 
 func getInput() []string {
 	var inputs []string
@@ -32,7 +40,15 @@ func getInput() []string {
 }
 
 func main() {
+	defer elapsed("AoC Day 8")()
 	instructions := getInput()
+	part1(instructions)
+	part2(instructions)
+}
+
+func part1(instructions []string) {
+	defer elapsed("Part 1")()
+
 	executedInstructions := make(map[int]string)
 
 	reversedInstructions := make(map[string]string)
@@ -58,7 +74,49 @@ func main() {
 			PC += modifier
 		}
 	}
-	fmt.Println("\nanswer p1", accumulator)
+	fmt.Println("Part 1:", accumulator)
+}
+
+func part2(instructions []string) {
+	defer elapsed("Part 2")()
+	reversedInstructions := make(map[string]string)
+	reversedInstructions["nop"] = "jmp"
+	reversedInstructions["jmp"] = "nop"
+	reversedInstructions["acc"] = "acc"
+
+	i := 0
+	for i < len(instructions) {
+		loop := false
+		executedInstructions := make(map[int]string)
+
+		PC := 0
+		accumulator := 0
+		for PC < len(instructions) {
+			instruction, modifier := parseInstruction(instructions[PC])
+			if executedInstructions[PC] != "" {
+				loop = true
+				break
+			}
+			executedInstructions[PC] = instructions[PC]
+			if PC == i {
+				instruction = reversedInstructions[instruction]
+			}
+
+			switch instruction {
+			case "nop":
+				PC += 1
+			case "acc":
+				PC += 1
+				accumulator += modifier
+			case "jmp":
+				PC += modifier
+			}
+		}
+		if !loop {
+			fmt.Println("Part 2:", accumulator)
+		}
+		i++
+	}
 }
 
 func parseInstruction(instr string) (string, int) {
